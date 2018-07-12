afe8e654357c14bee1b74499e1d1f7897f8f164d
Connor Unwin <connorunwin@gmail.com>
2018-07-12 22:35:12 +0100

    Resolved issue with getting wired value

--- get_network_info.sh
+++ get_network_info.sh
@@ -1,15 +1,21 @@
+#!/bin/bash
+
+# Variables
 wired=$(ipconfig getifaddr en0)
 wireless=$(ipconfig getifaddr en1)
-router=$(ipconfig getoption en0 router)
+router=$(ipconfig getoption en1 router)
 
 echo "----------NETWORK INFO---------"
 
-if [ -z "$ethernet" ]
+# Check if there is a wired connection
+if [ -z "$wired" ]
 then
 	echo "Ethernet: No IP Address Assigned"
 else
 	echo "Wired: $wired"
 fi
+
+# Check if there is a wireless connection
 if [ -z "$wireless" ]
 then
 	echo "Wireless: No IP Address Assigned"
